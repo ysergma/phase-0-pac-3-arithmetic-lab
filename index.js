527b358cb9e2a90a4757cc3d7df1856aa5edadaf
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-26 13:17:05 +0100

    completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+const add= function(a,b){
+    return a+b
+}
+
+const subtract= function(a,b){
+    return a-b
+}
+
+const multiply= function(a,b){
+    return a*b
+}
+
+const divide= function(a,b){
+    return a/b
+}
+
+const increment= function(n){
+     return ++n
+}
+increment(200);
+const decrement= function(n){
+   return --n;
+}
+
+const makeInt= function(n){
+    return parseInt(n,10)
+}
+
+const preserveDecimal= function(n){
+    return parseFloat((n))
+}
